88f4aa2a524f6b42e0ba3becc9bdae9c358fafde
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-11-11 21:38:26 -0300

    Criação do Header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import Header from '../../components/Header';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return <Header />;
 }
