4632b69e5ad42042d5bd45df65688fc03fb0f51b
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-11-13 20:25:19 -0300

    Configuração do Reactotron

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
+import './config/reactotron';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
-import store from './store';
+import store from './store/index';
 
 function App() {
   return (
